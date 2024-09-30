062820474629a818ad622ee71b31757b33c5e3f7
Tsungyu Yu <ytyubox@gmail.com>
2024-09-30 16:44:41 +0800

    --- Auto Git Commit ---

--- main.ts
+++ main.ts
@@ -17,10 +17,9 @@ export default class MyPlugin extends Plugin {
 		await this.loadSettings();
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
 		// Register an event listener that runs whenever the layout changes
 		this.registerEvent(this.app.workspace.on('layout-change', () => {
+			console.log('layout-change')
 			this.addRightClickContextMenu();
 		}));
 		// This adds a simple command that can be triggered anywhere
@@ -35,44 +34,6 @@ export default class MyPlugin extends Plugin {
 				console.log(selection);
 			}
 		});
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
 	}
 
 	onunload() {
